a1d5fd551e6c09d20204722d896acc0bfdf58b9c
VipulChauhan89 <vipulchauhan548@gmail.com>
2023-03-27 14:58:52 +0530

    Program to use frames to create personal portfolio

---
+++ Program-05/index.js
@@ -0,0 +1,35 @@
+function objective()
+{
+    parent.f3.document.getElementById("display").innerHTML="Interested in pursuing a career in the field of software development with a focus on web and app development as well as machine learning. I am a motivated and versatile developer who is passionate about creating visually appealing, user-friendly applications and building machine learning models.";
+    parent.f4.document.getElementById("image").src="img/1.png";
+}
+function personal()
+{
+    parent.f3.document.getElementById("display").innerHTML="Name : Vipul Chauhan</br>Email : Vipulchauhan548@gmail.com";
+    parent.f4.document.getElementById("image").src="img/2.png";
+}
+function family()
+{
+    parent.f3.document.getElementById("display").innerHTML="Father's Name : Mr. Brijmohan Singh Chauhan</br>Mother's Name : Mrs. Rachan Chauhan";
+    parent.f4.document.getElementById("image").src="img/3.png";
+}
+function education()
+{
+    parent.f3.document.getElementById("display").innerHTML="10th : Doon Public School Bhaniyawala</br>12th : Doon Public School Bhaniyawala</br>Graduation-B.tech(CSE) : Graphic Era Hill University Dehradun ";
+    parent.f4.document.getElementById("image").src="img/4.png";
+}
+function experience()
+{
+    parent.f3.document.getElementById("display").innerHTML="N/A";
+    parent.f4.document.getElementById("image").src="img/5.png";
+}
+function achievements()
+{
+    parent.f3.document.getElementById("display").innerHTML="Participated in the Grand finale of Smart India Hackathon 2022 as a Team Lead of our team of 6 members and we were runner-up in our problem statement competing among the top 5 team of India that were shortlisted for our problem statment in the finale.";
+    parent.f4.document.getElementById("image").src="img/6.png";
+}
+function others()
+{
+    parent.f3.document.getElementById("display").innerHTML="Others";
+    parent.f4.document.getElementById("image").src="img/1.png";
+}
